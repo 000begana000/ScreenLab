4391c49bedb26097b998d9823356ae637bb3e2ae
Begana <begana.contact@gmail.com>
2024-11-24 17:46:28 +0100

    stye: height of container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Carousel from "../components/Carousel.jsx";
 import Movies from "../components/Movies.jsx";
 
+import classes from "./Home.module.css";
+
 export default function HomePage() {
   return (
     <>
       <Carousel />
-      <div className="container">
+      <div className={classes.container}>
         <Movies />
       </div>
     </>
