bdaa478a224c21c15ee972d2c0020acdb75c2037
Begana <begana.contact@gmail.com>
2024-11-24 16:26:44 +0100

    feat: create nav bar

--- src/components/MainNaviation.jsx
+++ src/components/MainNaviation.jsx
@@ -1,15 +1,17 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function MainNaviation() {
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            {/* Home */}
-            <NavLink to="/">ScreenLab</NavLink>
-          </li>
-        </ul>
+      <nav className="nav">
+        <div className="logo">
+          <Link to="/">ScreenLab</Link>
+        </div>
+        <div className="user-actions">
+          <input type="text" name="" id="" />
+          <button className="user-avatar">Login</button>
+          <button className="language">EN</button>
+        </div>
       </nav>
     </header>
   );
