cf1959b995efdda13b9a9b831d6b99af8a08b2d7
Begana <begana.contact@gmail.com>
2024-11-24 16:34:03 +0100

    style: skeleton style

--- src/components/MainNaviation.jsx
+++ src/components/MainNaviation.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import classes from "./MainNaviation.module.css";
 
 export default function MainNaviation() {
   return (
     <header>
-      <nav className="nav">
+      <nav className={classes.nav}>
         <div className="logo">
-          <Link to="/">ScreenLab</Link>
+          <p>
+            <Link to="/">ScreenLab</Link>
+          </p>
         </div>
         <div className="user-actions">
           <input type="text" name="" id="" />
